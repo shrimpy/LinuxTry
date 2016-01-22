4cadb22bc004798de4be07b1dbc060c4ebec7462
Xiaomin Wu <xiaowu@microsoft.com>
2016-01-22 13:41:58 -0800

    Empty MVC 4 app

---
+++ Mvc4WebApiTry/WEBAPISample45/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WEBAPISample45.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET /api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
