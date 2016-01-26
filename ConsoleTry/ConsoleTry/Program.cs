5531b32cec4504f1dd271356c5f3f2edd68d024a
Xiaomin Wu <xiaowu@microsoft.com>
2016-01-25 16:56:58 -0800

    OS detector

--- ConsoleTry/ConsoleTry/Program.cs
+++ ConsoleTry/ConsoleTry/Program.cs
@@ -9,6 +9,26 @@ namespace ConsoleTry
     {
         static void Main(string[] args)
         {
+
+            OperatingSystem os = Environment.OSVersion;
+            PlatformID pid = os.Platform;
+            switch (pid)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    Console.WriteLine("Windows");
+                    break;
+                case PlatformID.Unix:
+                    Console.WriteLine("Unix");
+                    break;
+                default:
+                    Console.WriteLine("Others");
+                    break;
+            }
+
+            Console.WriteLine(Environment.SpecialFolder.ProgramFilesX86 + " " + Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86));
             Console.WriteLine(Environment.GetEnvironmentVariable("HOME"));
             Console.WriteLine("Run on linux");
         }
