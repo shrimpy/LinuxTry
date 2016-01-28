fa222b899f2d7b4dc708f87c02026ba7f3cc2e79
Xiaomin Wu <xiaowu@microsoft.com>
2016-01-27 16:37:25 -0800

    check point

--- ConsoleTry/ConsoleTry/Program.cs
+++ ConsoleTry/ConsoleTry/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace ConsoleTry
 {
@@ -9,28 +7,33 @@ namespace ConsoleTry
     {
         static void Main(string[] args)
         {
+            string content = @"#!/bin/sh
+echo hello world!
+";
 
-            OperatingSystem os = Environment.OSVersion;
-            PlatformID pid = os.Platform;
-            switch (pid)
-            {
-                case PlatformID.Win32NT:
-                case PlatformID.Win32S:
-                case PlatformID.Win32Windows:
-                case PlatformID.WinCE:
-                    Console.WriteLine("Windows");
-                    break;
-                case PlatformID.Unix:
-                    Console.WriteLine("Unix");
-                    break;
-                default:
-                    Console.WriteLine("Others");
-                    break;
-            }
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "tmp.sh"), content.Replace("\r\n", "\n"));
 
-            Console.WriteLine(Environment.SpecialFolder.ProgramFilesX86 + " " + Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86));
-            Console.WriteLine(Environment.GetEnvironmentVariable("HOME"));
-            Console.WriteLine("Run on linux");
+            //OperatingSystem os = Environment.OSVersion;
+            //PlatformID pid = os.Platform;
+            //switch (pid)
+            //{
+            //    case PlatformID.Win32NT:
+            //    case PlatformID.Win32S:
+            //    case PlatformID.Win32Windows:
+            //    case PlatformID.WinCE:
+            //        Console.WriteLine("Windows");
+            //        break;
+            //    case PlatformID.Unix:
+            //        Console.WriteLine("Unix");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Others");
+            //        break;
+            //}
+
+            //Console.WriteLine(Environment.SpecialFolder.ProgramFilesX86 + " " + Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86));
+            //Console.WriteLine(Environment.GetEnvironmentVariable("HOME"));
+            //Console.WriteLine("Run on linux");
         }
     }
 }
